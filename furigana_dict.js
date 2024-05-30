633275d65e5e5faee16475e01cd1980ebf78c79d
Daniel Chan <chanjdaniel@gmail.com>
2024-05-30 14:50:55 -0700

    finished adding grammar rules

--- furigana_dict.js
+++ furigana_dict.js
@@ -2,6 +2,7 @@ const jmdict = require('./JmdictFurigana.json');
 const furiganaDict = new Map();
 const FileSystem = require("fs");
 
+// extract patterns from dictionary
 for (let i = 0; i < jmdict.length; i++) {
     let entry = jmdict[i]["text"];
     let j = 0;
@@ -12,28 +13,89 @@ for (let i = 0; i < jmdict.length; i++) {
         if (containsKanji(group)) {
             if (!furiganaDict.has(group)) {
                 furiganaDict.set(group, new Map());
+                furiganaDict.get(group).set("pre", new Map());
+                furiganaDict.get(group).set("post", new Map());
             }
             let groupFurigana = getGroupFurigana(group, i);
-            let context = k + 1 < groupArray.length ? groupArray[k + 1] : "";
-            if (!furiganaDict.get(group).has(context)) {
-                furiganaDict.get(group).set(context, groupFurigana);
+            let precontext = k - 1 >= 0 ? groupArray[k - 1] : "";
+            let postcontext = k + 1 < groupArray.length ? groupArray[k + 1] : "";
+            if (!furiganaDict.get(group).get("post").has(postcontext)) {
+                furiganaDict.get(group).get("post").set(postcontext, groupFurigana);
+            }
+            if (!furiganaDict.get(group).get("pre").has(precontext) && precontext.length > 1) {
+                furiganaDict.get(group).get("pre").set(precontext, groupFurigana);
             }
         }
     }
 }
 
-let furiganaObject = Object.fromEntries(furiganaDict);
-let furiganaKeys = Object.keys(furiganaObject);
-for (let i = 0; i < furiganaKeys.length; i++) {
-    furiganaObject[furiganaKeys[i]] = Object.fromEntries(furiganaObject[furiganaKeys[i]]);
+// update dictionary with common conjugation rules
+for (let [kanji, context] of furiganaDict.entries()) {
+    for (let [postcontext, reading] of context.get("post").entries()) {
+        if (isVerbEnding(postcontext)) {
+            let teForm = getTeForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(teForm)) {
+                furiganaDict.get(kanji).get("post").set(teForm, reading);
+            }
+            let potentialTeForm = getPotentialTeForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(potentialTeForm)) {
+                furiganaDict.get(kanji).get("post").set(potentialTeForm, reading);
+            }
+            let potentialForm = getPotentialForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(potentialForm)) {
+                furiganaDict.get(kanji).get("post").set(potentialForm, reading);
+            }
+        }
+
+        if (postcontext == "い" || postcontext == "しい") {
+            let kuForm = getAdjKuForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(kuForm)) {
+                furiganaDict.get(kanji).get("post").set(kuForm, reading);
+            }
+
+            let condForm = getAdjCondForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(condForm)) {
+                furiganaDict.get(kanji).get("post").set(condForm, reading);
+            }
+
+            let souForm = getAdjSouForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(souForm)) {
+                furiganaDict.get(kanji).get("post").set(souForm, reading);
+            }
+
+            let saForm = getAdjSaForm(postcontext);
+            if (!furiganaDict.get(kanji).get("post").has(saForm)) {
+                furiganaDict.get(kanji).get("post").set(saForm, reading);
+            }
+        }
+    }
 }
 
-FileSystem.writeFile('furigana.json', JSON.stringify(furiganaObject), (error) => {
+// remove redundant dictionary entries (does that make the program slower??)
+// for (let [kanji, context] of furiganaDict.entries()) {
+//     for (let [postcontext, reading] of context.get("post").entries()) {
+        
+//     }
+// }
+
+let furiganaObject = Object.fromEntries(furiganaDict);
+let kanjiKeys = Object.keys(furiganaObject);
+for (let i = 0; i < kanjiKeys.length; i++) {
+    let kanjiKey = kanjiKeys[i];
+    furiganaObject[kanjiKey] = Object.fromEntries(furiganaObject[kanjiKey]);
+    let furiganaKeys = Object.keys(furiganaObject[kanjiKey]);
+    for (let j = 0; j < furiganaKeys.length; j++) {
+        let furiganaKey = furiganaKeys[j];
+        furiganaObject[kanjiKey][furiganaKey] = Object.fromEntries(furiganaObject[kanjiKey][furiganaKey]);
+    }
+}
+
+FileSystem.writeFile('furiganaDict.json', JSON.stringify(furiganaObject), (error) => {
 if (error) throw error;
 });
 
-let test = "気";
-console.log(furiganaObject[test]);
+
+// HELPERS
 
 // returns string containing furigana reading of kanji group using jmdict
 // example: "阿吽" -> "あうん"
@@ -75,4 +137,128 @@ function getGroupArray(entry) {
 function containsKanji(text) {
     const re = /[\u4e00-\u9faf\u3400-\u4dbf]/;
     return re.test(text);
+}
+
+function isVerbEnding(text) {
+    const re = /^(?:く|ぐ|む|ぶ|ぬ|る|う|つ|す|しむ|じむ|きる|じる|[けげめべねれえてせ]る)$/;
+    return re.test(text);
+}
+
+function getPotentialForm(postcontext) {
+    let teForm = ""
+    let verbEnding = postcontext[0];
+    if (postcontext.length == 2) {
+        verbEnding = postcontext[1];
+        teForm += postcontext[0];
+    }
+    if (verbEnding == "く") {
+        teForm += "ける";
+    } else if (verbEnding == "ぐ") {
+        teForm += "げる";
+    } else if (verbEnding == "む") {
+        teForm += "める";
+    } else if (verbEnding == "ぶ") {
+        teForm += "べる";
+    } else if (verbEnding == "ぬ") {
+        teForm += "ねる";
+    } else if (verbEnding == "る") {
+        teForm += "れる";
+    } else if (verbEnding == "う") {
+        teForm += "える";
+    } else if (verbEnding == "つ") {
+        teForm += "てる";
+    } else if (verbEnding == "す") {
+        teForm += "せる";
+    }
+    return teForm;
+}
+
+function getPotentialTeForm(postcontext) {
+    let teForm = ""
+    let verbEnding = postcontext[0];
+    if (postcontext.length == 2) {
+        verbEnding = postcontext[1];
+        teForm += postcontext[0];
+    }
+    if (verbEnding == "く") {
+        teForm += "けて";
+    } else if (verbEnding == "ぐ") {
+        teForm += "げて";
+    } else if (verbEnding == "む") {
+        teForm += "めて";
+    } else if (verbEnding == "ぶ") {
+        teForm += "べて";
+    } else if (verbEnding == "ぬ") {
+        teForm += "ねて";
+    } else if (verbEnding == "る") {
+        teForm += "れて";
+    } else if (verbEnding == "う") {
+        teForm += "えて";
+    } else if (verbEnding == "つ") {
+        teForm += "てて";
+    } else if (verbEnding == "す") {
+        teForm += "せて";
+    }
+    return teForm;
+}
+
+function getTeForm(postcontext) {
+    let teForm = ""
+    let verbEnding = postcontext[0];
+    if (postcontext.length == 2) {
+        verbEnding = postcontext[1];
+        teForm += postcontext[0];
+    }
+    if (verbEnding == "く") {
+        teForm += "いて";
+    } else if (verbEnding == "ぐ") {
+        teForm += "いで";
+    } else if (verbEnding == "む") {
+        teForm += "んで";
+    } else if (verbEnding == "ぶ") {
+        teForm += "んで";
+    } else if (verbEnding == "ぬ") {
+        teForm += "んで";
+    } else if (verbEnding == "る") {
+        teForm += "って";
+    } else if (verbEnding == "う") {
+        teForm += "って";
+    } else if (verbEnding == "つ") {
+        teForm += "って";
+    } else if (verbEnding == "す") {
+        teForm += "して";
+    }
+    return teForm;
+}
+
+function getAdjKuForm(adjEnding) {
+    if (adjEnding == "い") {
+        return "く";
+    } else if (adjEnding == "しい") {
+        return "しく";
+    }
+}
+
+function getAdjCondForm(adjEnding) {
+    if (adjEnding == "い") {
+        return "ければ";
+    } else if (adjEnding == "しい") {
+        return "しければ";
+    }
+}
+
+function getAdjSouForm(adjEnding) {
+    if (adjEnding == "い") {
+        return "そう";
+    } else if (adjEnding == "しい") {
+        return "しそう";
+    }
+}
+
+function getAdjSaForm(adjEnding) {
+    if (adjEnding == "い") {
+        return "さ";
+    } else if (adjEnding == "しい") {
+        return "しさ";  
+    }
 }
